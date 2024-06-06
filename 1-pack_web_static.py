c890b1d07ae8870a17d1f6fcc5a1f36614b8e2dd
soukainanachib <soukaina.nachib23@gmail.com>
2024-06-06 18:07:50 +0100

    modifie adress ip of servers

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
-from fabric.api import local
+from fabric import task
 from time import strftime
-from datetime import date
+from datetime import datetime
 
+@task
+def do_pack(c):
+    """Generates a .tgz archive from the contents of the web_static folder."""
+    local = c.local  # Adjust for Fabric 3.x
+    
+    # Create the versions directory if it doesn't exist
+    local("mkdir -p versions")
 
-def do_pack():
-    """ A script that generates archive the contents of web_static folder"""
+    # Create the archive name with the current date and time
+    now = datetime.now()
+    archive_name = "versions/web_static_{}.tgz".format(now.strftime("%Y%m%d%H%M%S"))
 
-    filename = strftime("%Y%m%d%H%M%S")
+    # Print message indicating the start of the packing process
+    print("Packing web_static to {}".format(archive_name))
+
+    # Create the archive
     try:
-        local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(filename))
-
-        return "versions/web_static_{}.tgz".format(filename)
-
+        local("tar -cvzf {} web_static".format(archive_name))
+        return archive_name
     except Exception as e:
+        print("An error occurred while creating the archive:", e)
         return None
+
